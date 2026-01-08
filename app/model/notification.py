95da70e865f42fcba4faeb6dc91be46660a9b02b
Ehsan Ullah <ihsan42180@gmail.com>
2026-01-08 22:42:42 +0500

    Add status column is_read

--- app/model/notification.py
+++ app/model/notification.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
 from sqlalchemy.sql import func
 from app.db.database import Base
 
@@ -9,4 +9,5 @@ class Notification(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey('usersinfo.id'), nullable=False)
     message = Column(String, nullable=False)
+    is_read = Column(Boolean, default=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
